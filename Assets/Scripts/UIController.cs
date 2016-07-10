f508245bedd03c0102cdf1d32061de5bde5047bf
Boris Kostadinov <kostadinov.boris@gmail.com>
2016-07-10 14:48:26 +0300

    New UI contoller class.

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+	/* UI Controller singleton */
+	public static UIController instance { get; private set; }
+
+	/* System function. */
+	void Awake() {
+		// make the UI controller a singleton:
+		instance = this;
+	}
+
+	/* Used to exit the game application. */
+	public void ExitGame() {
+		Application.Quit();
+	}
+}
